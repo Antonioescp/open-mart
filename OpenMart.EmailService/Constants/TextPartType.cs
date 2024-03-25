11cb41c41c179777c89536800da27ac62c1c440d
Antonio Escorcia <packageinstaller2@gmail.com>
2024-03-24 19:14:09 -0600

    Added Email service

---
+++ OpenMart.EmailService/Constants/TextPartType.cs
@@ -0,0 +1,13 @@
+﻿using OpenMart.ExtraSharp.Enums;
+
+namespace OpenMart.EmailService.Constants;
+
+public class TextPartType : Enumeration<int, string>
+{
+    public static TextPartType Plain => new(0, "plain");
+    public static TextPartType Html => new(1, "html");
+    
+    private TextPartType(int id, string value) : base(id, value)
+    {
+    }
+}
